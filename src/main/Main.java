aeaad3326aca2c558b5dd7ea306b102f2fc1172a
unknown <SHESTAU_A@IBA>
2020-03-03 13:09:56 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+/**
+ * User: Shestau
+ * Date: 03.03.2020
+ */
+
+public class Main {
+    /**
+     * First java program
+     * @param args: program arguments
+     */
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello!");
+    }
+}
---
+++ src/main/New.java
@@ -0,0 +1,7 @@
+package main;
+/**
+ * User: Shestau
+ * Date: 03.03.2020
+ */
+public class New {
+}
